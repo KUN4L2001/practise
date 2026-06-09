43204e633d14c8a4500ddc84278672538dbdb599
Kunal Gaikwad <kunal.gaikwad.tb@billdesk.com>
2026-06-09 14:52:31 +0530

    Excel file processing

--- src/main/java/com/myapp/practise/camel/processor/FileProcessor.java
+++ src/main/java/com/myapp/practise/camel/processor/FileProcessor.java
@@ -1,8 +1,16 @@
 package com.myapp.practise.camel.processor;
 
+import java.io.File;
+
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -10,8 +18,36 @@ import org.springframework.stereotype.Component;
 public class FileProcessor implements Processor {
 	@Override
 	public void process(Exchange exchange) throws Exception {
-		String content = exchange.getIn().getBody(String.class);
-		log.info("Processing: {}", content);
-		exchange.getMessage().setBody(content.toUpperCase());
+		File file = exchange.getIn().getBody(File.class);
+		log.info("Path: {}", file.getAbsolutePath());
+		log.info("Size: {}", file.length());
+		try(Workbook workbook = WorkbookFactory.create(file)){
+			Sheet sheet = workbook.getSheetAt(0);
+			for(Row row : sheet){
+				if(isRowEmpty(row)){
+					continue;
+				}
+				for(Cell cell : row){
+					if (cell.getCellType() == CellType.BLANK ||
+							cell.toString().trim().isEmpty()) {
+						continue;
+					}
+					log.info("Row={}, Cell={}, Value={}",row.getRowNum(), cell.getColumnIndex(), cell.toString());
+				}
+			}
+		}
+	}
+
+	private boolean isRowEmpty(Row row){
+		if(row == null){
+			return true;
+		}
+
+		for(Cell cell : row){
+			if(cell.getCellType() != CellType.BLANK && !cell.toString().trim().isEmpty()){
+				return false;
+			}
+		}
+		return true;
 	}
 }
